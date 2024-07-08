e827fccb8ef32d5a797e255af7fd7f85ff1e8fb7
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-07-08 10:28:05 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-// write code here
+const array = [
+  { name: 'Ben', age: 14 },
+  { name: 'Phil', age: 18 },
+  { name: 'John', age: 32 },
+  { name: 'Ann', age: 16 },
+  { name: 'Paul', age: 24 }
+];
+// อายุไม่น้อยกว่า 18 
+const result = array.filter(obj => obj.age >= 18)
